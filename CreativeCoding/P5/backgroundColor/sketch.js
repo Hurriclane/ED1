0ae7ec5aed8ee95d1e6afc3417a879239f0e9a5c
Hurriclane <laneybugrussell@gmail.com>
2021-11-16 14:09:23 -0500

    Update sketch.js

--- CreativeCoding/P5/backgroundColor/sketch.js
+++ CreativeCoding/P5/backgroundColor/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
- 
+createCanvas(windoWidth, windowHight); this creates the window width and hight to be the size of my browser
+background (179,230,198);
 }
 
 function draw() {
-  
+
 }
