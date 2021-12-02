9c5aaf9344c67e03be74f73a0bb43606c15aa0ca
Hurriclane <laneybugrussell@gmail.com>
2021-12-02 11:45:23 -0500

    troubleshooting

--- CreativeCoding/P5/backgroundColor/sketch.js
+++ CreativeCoding/P5/backgroundColor/sketch.js
@@ -1,20 +1,57 @@
+// let gif;
+// function preload() {
+//   gif = loadImage('assets/aiBG1.gif');
+// }
+//
+//
+//
+// function setup() { //runs once
+//   createCanvas(windowWidth,windowHeight);
+//   fill (149, 185, 237);
+//   strokeWeight(1)
+// }
+//
+// function draw() {//runs in a loop
+//       image (gif,0,0);
+// // var num=10;
+// //   var sideLen = windowWidth/num;
+// //
+// //   for (var y = 0;y < windowHeight; y = y + sideLen){
+// //     for (var x = 0; x < windowWidth; x = x + sideLen){
+// //       image (gif,x,y);
+// //     }
+// //   }
+//
+//
+// }
+//
+let gif;
 
-function setup() { //runs once
-  createCanvas(windowWidth,windowHeight);
-  fill (149, 185, 237);
-  strokeWeight(1)
+function preload (){
+  gif = loadImage ('assets/aiBG1.gif');
 }
 
-function draw() {//runs in a loop
+function setup() { //runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('indigo');
+  strokeWeight(1);
+}
 
-var num=10;
-  var sideLen = windowWidth/num;
+function draw() {
 
-  for (var y = 0;y < windowHeight; y = y + sideLen)
-    for (var x = 0; x < windowWidth; x = x + sideLen)
-  quad(x,y,
-      x + sideLen, y,
-      x + sideLen, y + sideLen,
-      x, y + sideLen)
+var num = 10; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
 
+  for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+image (gif, x, y);
+  }
+}
+
+//noLoop ();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
