0437db4612ad8acbecb0cf19b6b0bc44be335d0d
Hurriclane <laneybugrussell@gmail.com>
2022-01-10 19:47:02 -0500

    Update sketch.js

--- CreativeCoding/P5/backgroundColor2/sketch.js
+++ CreativeCoding/P5/backgroundColor2/sketch.js
@@ -18,7 +18,11 @@ var sideLen = windowWidth/num;//variable for the side lenth of each square
 
   for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
      for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
-image (gif, x, y);
+       image (gif, x, y
+          x + sideLen, y,
+          x + sideLen. y + sideLen,
+          x, y + sideLen);
+
   }
 }
 
